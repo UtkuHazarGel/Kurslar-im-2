6d774248c0bd44720b0e053c616f74968dc7645b
Utku <utkuhazargel@gmail.com>
2024-01-31 03:16:48 +0300

    kurs listeleme ekranının tasarımı yapıldı

--- screens/AllCourses.js
+++ screens/AllCourses.js
@@ -1,12 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Courses from '../components/Courses'
 
 export default function AllCourses() {
   return (
-    <View>
-      <Text>AllCourses</Text>
+    <View style={styles.container}>
+      <Courses coursesPeriod="Hepsi"/>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:"center",
+        textAlign:"center"
+    }
+})
--- screens/RecentCourses.js
+++ screens/RecentCourses.js
@@ -1,12 +1,19 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import Courses from '../components/Courses'
 
 export default function RecentCourses() {
   return (
-    <View>
-      <Text>RecentCourses</Text>
+    <View style={styles.container}>
+      <Courses coursesPeriod="Son Bir Hafta"/>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:"center",
+        textAlign:"center"
+    }
+})
